408ab225bdddc295541eb3dcd798ff605b9db201
Lilinamay <82954244+Lilinamay@users.noreply.github.com>
2022-10-20 21:57:27 -0400

    change

---
+++ snakeGame/Assets/Scripts/Snake.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Snake : MonoBehaviour
+{
+    private List<Transform> segments = new List<Transform>();
+    public Transform segmentPrefab;
+    public Vector2 direction = Vector2.right;
+    private Vector2 input;
+    public int initialSize = 1;
+
+    private Vector2 startPos;
+    public int pixelDistToDetect = 20;
+    private bool fingerDown;
+    float speed = 0.5f;
+    private void Start()
+    {
+        ResetState();
+    }
+
+    private void Update()
+    {
+
+        if (fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            startPos = Input.touches[0].position;
+            fingerDown = true;
+        }
+        if (fingerDown && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended)
+        {
+            //startPos will be reset
+            fingerDown = false;
+        }
+
+        //TOUCH TEST CODE
+        /*
+        if (fingerDown)
+        {
+            //Did we swipe up?
+            if (Input.touches[0].position.y >= startPos.y + pixelDistToDetect)
+            {
+                fingerDown = false;
+                Debug.Log("Swipe up");
+            }
+            else if (Input.touches[0].position.y <= startPos.y - pixelDistToDetect)
+            {
+                fingerDown = false;
+                Debug.Log("Swipe down");
+            }
+            else if (Input.touches[0].position.x <= startPos.x - pixelDistToDetect)
+            {
+                fingerDown = false;
+                Debug.Log("Swipe left");
+            }
+            //Did we swipe right?
+            else if (Input.touches[0].position.x >= startPos.x + pixelDistToDetect)
+            {
+                fingerDown = false;
+                Debug.Log("Swipe right");
+            }
+        }*/
+
+        //TOUCH CONTROLS
+        // Only allow turning up or down while moving in the x-axis
+        if (direction.x != 0f)
+        {
+            if(fingerDown){
+            if (Input.touches[0].position.y >= startPos.y + pixelDistToDetect){
+                fingerDown = false;
+                input = Vector2.up;
+            } else if (Input.touches[0].position.y <= startPos.y - pixelDistToDetect) {
+                fingerDown = false;
+                input = Vector2.down;
+            }
+        }}
+        // Only allow turning left or right while moving in the y-axis
+        else if (direction.y != 0f)
+        {
+            if(fingerDown){
+            if (Input.touches[0].position.x >= startPos.x + pixelDistToDetect) {
+                fingerDown = false;
+                input = Vector2.right;
+            } else if (Input.touches[0].position.x <= startPos.x - pixelDistToDetect) {
+                fingerDown = false;
+                input = Vector2.left;
+            }}
+        }
+
+        /*KEYBOARD CONTROLS
+        // Only allow turning up or down while moving in the x-axis
+        if (direction.x != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+                input = Vector2.up;
+            } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+                input = Vector2.down;
+            }
+        }
+        // Only allow turning left or right while moving in the y-axis
+        else if (direction.y != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+                input = Vector2.right;
+            } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+                input = Vector2.left;
+            }
+        }*/
+    }
+
+    private void FixedUpdate()
+    {
+        // Set the new direction based on the input
+        if (input != Vector2.zero) {
+            direction = input;
+        }
+
+        // Set each segment's position to be the same as the one it follows. We
+        // must do this in reverse order so the position is set to the previous
+        // position, otherwise they will all be stacked on top of each other.
+        for (int i = segments.Count - 1; i > 0; i--) {
+            segments[i].position = segments[i - 1].position;
+        }
+
+        // Move the snake in the direction it is facing
+        // Round the values to ensure it aligns to the grid
+        //float x = Mathf.Round(transform.position.x) + direction.x*speed;
+        //float y = Mathf.Round(transform.position.y) + direction.y * speed;
+        float x = Mathf.Round(transform.position.x) + direction.x;
+        float y = Mathf.Round(transform.position.y) + direction.y;
+
+        transform.position = new Vector2(x, y);
+    }
+
+    public void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+    public void ResetState()
+    {
+        direction = Vector2.right;
+        transform.position = Vector3.zero;
+
+        // Start at 1 to skip destroying the head
+        for (int i = 1; i < segments.Count; i++) {
+            Destroy(segments[i].gameObject);
+        }
+
+        // Clear the list but add back this as the head
+        segments.Clear();
+        segments.Add(transform);
+
+        // -1 since the head is already in the list
+        for (int i = 0; i < initialSize - 1; i++) {
+            Grow();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food")) {
+            Grow();
+        } else if (other.gameObject.CompareTag("Obstacle")) {
+            ResetState();
+        }
+    }
+
+}
